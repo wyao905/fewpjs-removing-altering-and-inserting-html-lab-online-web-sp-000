ec017904bf2afbd817f496caa14f5c2d8b7e851a
Warren Yao <warrenyca905@gmail.com>
2020-01-14 04:52:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+main.remove();
+
+let newHeader = document.createElement('h1');
+newHeader.id = "victory"
+newHeader.innerHTML = "YOUR-NAME is the champion";
+document.body.appendChild(newHeader);
