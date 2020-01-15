ed2fca06e630979506a27957d0511024d9b4fac3
Warren Yao <warrenyca905@gmail.com>
2020-01-15 04:40:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 // Write your code here!
 main.remove();
 
+<<<<<<< HEAD
 let newHeader = document.createElement('h1');
 newHeader.id = "victory"
 newHeader.innerHTML = "YOUR-NAME is the champion";
 document.body.appendChild(newHeader);
+=======
+function newHeader(name){
+  let a = document.createElement('h1#victory');
+  a.innerHTML = `${name} is the champion`;
+  document.body.appendchild(a);
+}
+>>>>>>> 633884d274ea70e7d2a409327e6f233ce105e491
